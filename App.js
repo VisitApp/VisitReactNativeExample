4002d05b42952cd351bf9f7139118d5dabd57f73
chinmay <chinmaydash11@gmail.com>
2022-06-20 11:46:45 +0530

    tested android plugin

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import VisitHealthView from 'react-native-visit-health-rn';
+
+
+// ...
+
+
+export default function App() {
+  return <VisitHealthView source="https://star-health.getvisitapp.xyz/login" />;
+}
